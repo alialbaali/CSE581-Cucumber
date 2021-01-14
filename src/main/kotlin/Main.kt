e1314c5624c291df72b842e3b4c477446ed7a791
Ali Albaali <ali@albaali.com>
2021-01-14 21:19:31 +0300

    Add WebDriver setup function

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,25 @@
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.firefox.FirefoxDriver
+import java.util.concurrent.TimeUnit
+
+
 fun main(args: Array<String>) {
     println("Hello World!")
 }
+
+fun setupWebDriver(): WebDriver {
+    System.setProperty(
+        "webdriver.gecko.driver",
+        "C:\\Users\\ali\\Downloads\\geckodriver\\geckodriver.exe"
+    )
+    return FirefoxDriver().apply {
+        manage()
+            .window()
+            .maximize()
+
+        manage()
+            .timeouts()
+            .implicitlyWait(10, TimeUnit.SECONDS)
+    }
+}
+
